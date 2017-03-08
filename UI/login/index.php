b5a136176eecf7ea52e4567644d8b80b41c5da3f
rematchka <reem.abdelsalam13@gmail.com>
2017-03-08 12:17:31 +0200

    sign in and sign up

---
+++ UI/login/index.php
@@ -0,0 +1,65 @@
+<div id="login" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <h4 class="modal-title"SIGN UP</h4>
+      </div>
+    
+        
+         <div class="modal-body">
+                
+                <form class="form-horizontal" role="form">
+                  <div class="form-group">
+                    <label  class="col-sm-2 control-label"
+                              for="inputEmail3">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" 
+                        id="inputEmail3" placeholder="Email"  required/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="col-sm-2 control-label"
+                          for="inputPassword3" >Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control"
+                            id="inputPassword3" placeholder="Password"  required/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                            <input type="checkbox"/> Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <a href="#" data-toggle="modal" style="color:black;" role="button">Forget Password?</a>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-default">Sign in</button>
+                    </div>
+                  </div>
+                </form>
+                
+                
+                
+                
+                
+                
+           
+      </div>
+      <div class="modal-footer">
+       
+      </div>
+    </div>
+
+  </div>
+</div>
---
+++ UI/signup/index.php
@@ -0,0 +1,76 @@
+<div id="signup" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        
+        <h4 class="modal-title"SIGN UP</h4>
+      </div>
+    
+        
+         <div class="modal-body">
+                
+                <form class="form-horizontal" role="form">
+
+
+                 <div class="form-group">
+                  
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control"
+                            id="fname" placeholder="First Name"  required/>
+                    </div>
+                  </div>
+                   <div class="form-group">
+                  
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control"
+                            id="lname" placeholder="Last Name"  required/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                   
+
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" 
+                        id="inputEmail3" placeholder="Email"  required/>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control"
+                            id="inputPassword3" placeholder="Password"  required/>
+                    </div>
+                  </div>
+                 
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                            <input type="checkbox"/> Remember me
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-default">Sign Up</button>
+                    </div>
+                  </div>
+                </form>
+                
+                
+                
+                
+                
+                
+           
+      </div>
+      <div class="modal-footer">
+       
+      </div>
+    </div>
+
+  </div>
+</div>
